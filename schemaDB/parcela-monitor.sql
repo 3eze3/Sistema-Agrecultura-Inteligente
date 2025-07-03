576cc7218896785f599be19b98c4ee704158075c
3eze3 <ezekics33@gmail.com>
2025-07-02 23:31:20 -0500

    Add the sql script to execute the data base

--- schemaDB/parcela-monitor.sql
+++ schemaDB/parcela-monitor.sql
@@ -1,3 +1,4 @@
+--- Crear la talba 
 CREATE TABLE IF NOT EXISTS lecturas (
   id SERIAL PRIMARY KEY,
   timestamp TIMESTAMP NOT NULL,
@@ -7,4 +8,11 @@ CREATE TABLE IF NOT EXISTS lecturas (
 
 SELECT * FROM lecturas;
 
+--- Filtrado por fechas sip.
+SELECT porcentaje,timestamp FROM lecturas
+WHERE timestamp >= '2025-06-27 16:30:00'  ---  (Donde?? Logica)
+  AND timestamp <= '2025-06-27 16:45:00'
+ORDER BY timestamp ASC; --- (Quiero que el orden que es muester el resultado de arriba sea en: X,Y,Z...)
+
+--- Un clear de la tabal (Reniciar los datos y los id tambine)
 TRUNCATE TABLE lecturas RESTART IDENTITY;
